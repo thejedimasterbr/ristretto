fb3fcf3d9792b92826c72ecfb55e8a2a96f857d0
thejedimasterbr <22400989+thejedimasterbr@users.noreply.github.com>
2025-03-03 06:32:35 -0300

    Update file.go

--- z/file.go
+++ z/file.go
@@ -6,305 +6,202 @@
 package z
 
 import (
+	"encoding/binary"
 	"errors"
 	"fmt"
-	"log"
+	"io"
 	"os"
-	"os/user"
 	"path/filepath"
-	"sort"
-	"strconv"
-	"strings"
-	"time"
 )
 
-// SuperFlagHelp makes it really easy to generate command line `--help` output for a SuperFlag. For
-// example:
-//
-//	const flagDefaults = `enabled=true; path=some/path;`
-//
-//	var help string = z.NewSuperFlagHelp(flagDefaults).
-//		Flag("enabled", "Turns on <something>.").
-//		Flag("path", "The path to <something>.").
-//		Flag("another", "Not present in defaults, but still included.").
-//		String()
-//
-// The `help` string would then contain:
-//
-//	enabled=true; Turns on <something>.
-//	path=some/path; The path to <something>.
-//	another=; Not present in defaults, but still included.
-//
-// All flags are sorted alphabetically for consistent `--help` output. Flags with default values are
-// placed at the top, and everything else goes under.
-type SuperFlagHelp struct {
-	head     string
-	defaults *SuperFlag
-	flags    map[string]string
+// MmapFile represents an mmapd file and includes both the buffer to the data
+// and the file descriptor.
+type MmapFile struct {
+	Data []byte
+	Fd   *os.File
 }
 
-func NewSuperFlagHelp(defaults string) *SuperFlagHelp {
-	return &SuperFlagHelp{
-		defaults: NewSuperFlag(defaults),
-		flags:    make(map[string]string, 0),
-	}
-}
+var NewFile = errors.New("Create a new file")
 
-func (h *SuperFlagHelp) Head(head string) *SuperFlagHelp {
-	h.head = head
-	return h
-}
-
-func (h *SuperFlagHelp) Flag(name, description string) *SuperFlagHelp {
-	h.flags[name] = description
-	return h
-}
-
-func (h *SuperFlagHelp) String() string {
-	defaultLines := make([]string, 0)
-	otherLines := make([]string, 0)
-	for name, help := range h.flags {
-		val, found := h.defaults.m[name]
-		line := fmt.Sprintf("    %s=%s; %s\n", name, val, help)
-		if found {
-			defaultLines = append(defaultLines, line)
-		} else {
-			otherLines = append(otherLines, line)
-		}
-	}
-	sort.Strings(defaultLines)
-	sort.Strings(otherLines)
-	dls := strings.Join(defaultLines, "")
-	ols := strings.Join(otherLines, "")
-	if len(h.defaults.m) == 0 && len(ols) == 0 {
-		// remove last newline
-		dls = dls[:len(dls)-1]
-	}
-	// remove last newline
-	if len(h.defaults.m) == 0 && len(ols) > 1 {
-		ols = ols[:len(ols)-1]
-	}
-	return h.head + "\n" + dls + ols
-}
-
-func parseFlag(flag string) (map[string]string, error) {
-	kvm := make(map[string]string)
-	for _, kv := range strings.Split(flag, ";") {
-		if strings.TrimSpace(kv) == "" {
-			continue
-		}
-		// For a non-empty separator, 0 < len(splits) ≤ 2.
-		splits := strings.SplitN(kv, "=", 2)
-		k := strings.TrimSpace(splits[0])
-		if len(splits) < 2 {
-			return nil, fmt.Errorf("superflag: missing value for '%s' in flag: %s", k, flag)
-		}
-		k = strings.ToLower(k)
-		k = strings.ReplaceAll(k, "_", "-")
-		kvm[k] = strings.TrimSpace(splits[1])
-	}
-	return kvm, nil
-}
-
-type SuperFlag struct {
-	m map[string]string
-}
-
-func NewSuperFlag(flag string) *SuperFlag {
-	sf, err := newSuperFlagImpl(flag)
+func OpenMmapFileUsing(fd *os.File, sz int, writable bool) (*MmapFile, error) {
+	filename := fd.Name()
+	fi, err := fd.Stat()
 	if err != nil {
-		log.Fatal(err)
+		return nil, errors.Join(err, fmt.Errorf("cannot stat file: %s", filename))
 	}
-	return sf
-}
 
-func newSuperFlagImpl(flag string) (*SuperFlag, error) {
-	m, err := parseFlag(flag)
+	var rerr error
+	fileSize := fi.Size()
+	if sz > 0 && fileSize == 0 {
+		// If file is empty, truncate it to sz.
+		if err := fd.Truncate(int64(sz)); err != nil {
+			return nil, errors.Join(err, errors.New("error while truncation"))
+		}
+		fileSize = int64(sz)
+		rerr = NewFile
+	}
+
+	// fmt.Printf("Mmaping file: %s with writable: %v filesize: %d\n", fd.Name(), writable, fileSize)
+	buf, err := Mmap(fd, writable, fileSize) // Mmap up to file size.
 	if err != nil {
-		return nil, err
+		return nil, errors.Join(err, fmt.Errorf("while mmapping %s with size: %d", fd.Name(), fileSize))
 	}
-	return &SuperFlag{m}, nil
-}
 
-func (sf *SuperFlag) String() string {
-	if sf == nil {
-		return ""
-	}
-	kvs := make([]string, 0, len(sf.m))
-	for k, v := range sf.m {
-		kvs = append(kvs, fmt.Sprintf("%s=%s", k, v))
-	}
-	return strings.Join(kvs, "; ")
-}
-
-func (sf *SuperFlag) MergeAndCheckDefault(flag string) *SuperFlag {
-	sf, err := sf.mergeAndCheckDefaultImpl(flag)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return sf
-}
-
-func (sf *SuperFlag) mergeAndCheckDefaultImpl(flag string) (*SuperFlag, error) {
-	if sf == nil {
-		m, err := parseFlag(flag)
-		if err != nil {
+	if fileSize == 0 {
+		dir, _ := filepath.Split(filename)
+		if err := SyncDir(dir); err != nil {
 			return nil, err
 		}
-		return &SuperFlag{m}, nil
 	}
+	return &MmapFile{
+		Data: buf,
+		Fd:   fd,
+	}, rerr
+}
 
-	src, err := parseFlag(flag)
+// OpenMmapFile opens an existing file or creates a new file. If the file is
+// created, it would truncate the file to maxSz. In both cases, it would mmap
+// the file to maxSz and returned it. In case the file is created, z.NewFile is
+// returned.
+func OpenMmapFile(filename string, flag int, maxSz int) (*MmapFile, error) {
+	// fmt.Printf("opening file %s with flag: %v\n", filename, flag)
+	fd, err := os.OpenFile(filename, flag, 0666)
 	if err != nil {
-		return nil, err
+		return nil, errors.Join(err, fmt.Errorf("unable to open: %s", filename))
 	}
+	writable := true
+	if flag == os.O_RDONLY {
+		writable = false
+	}
+	return OpenMmapFileUsing(fd, maxSz, writable)
+}
 
-	numKeys := len(sf.m)
-	for k := range src {
-		if _, ok := sf.m[k]; ok {
-			numKeys--
+type mmapReader struct {
+	Data   []byte
+	offset int
+}
+
+func (mr *mmapReader) Read(buf []byte) (int, error) {
+	if mr.offset > len(mr.Data) {
+		return 0, io.EOF
+	}
+	n := copy(buf, mr.Data[mr.offset:])
+	mr.offset += n
+	if n < len(buf) {
+		return n, io.EOF
+	}
+	return n, nil
+}
+
+func (m *MmapFile) NewReader(offset int) io.Reader {
+	return &mmapReader{
+		Data:   m.Data,
+		offset: offset,
+	}
+}
+
+// Bytes returns data starting from offset off of size sz. If there's not enough data, it would
+// return nil slice and io.EOF.
+func (m *MmapFile) Bytes(off, sz int) ([]byte, error) {
+	if len(m.Data[off:]) < sz {
+		return nil, io.EOF
+	}
+	return m.Data[off : off+sz], nil
+}
+
+// Slice returns the slice at the given offset.
+func (m *MmapFile) Slice(offset int) []byte {
+	sz := binary.BigEndian.Uint32(m.Data[offset:])
+	start := offset + 4
+	next := start + int(sz)
+	if next > len(m.Data) {
+		return []byte{}
+	}
+	res := m.Data[start:next]
+	return res
+}
+
+// AllocateSlice allocates a slice of the given size at the given offset.
+func (m *MmapFile) AllocateSlice(sz, offset int) ([]byte, int, error) {
+	start := offset + 4
+
+	// If the file is too small, double its size or increase it by 1GB, whichever is smaller.
+	if start+sz > len(m.Data) {
+		const oneGB = 1 << 30
+		growBy := len(m.Data)
+		if growBy > oneGB {
+			growBy = oneGB
+		}
+		if growBy < sz+4 {
+			growBy = sz + 4
+		}
+		if err := m.Truncate(int64(len(m.Data) + growBy)); err != nil {
+			return nil, 0, err
 		}
 	}
-	if numKeys != 0 {
-		return nil, fmt.Errorf("superflag: found invalid options: %s.\nvalid options: %v", sf, flag)
+
+	binary.BigEndian.PutUint32(m.Data[offset:], uint32(sz))
+	return m.Data[start : start+sz], start + sz, nil
+}
+
+func (m *MmapFile) Sync() error {
+	if m == nil {
+		return nil
 	}
-	for k, v := range src {
-		if _, ok := sf.m[k]; !ok {
-			sf.m[k] = v
+	return Msync(m.Data)
+}
+
+func (m *MmapFile) Delete() error {
+	// Badger can set the m.Data directly, without setting any Fd. In that case, this should be a
+	// NOOP.
+	if m.Fd == nil {
+		return nil
+	}
+
+	if err := Munmap(m.Data); err != nil {
+		return fmt.Errorf("while munmap file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	m.Data = nil
+	if err := m.Fd.Truncate(0); err != nil {
+		return fmt.Errorf("while truncate file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	if err := m.Fd.Close(); err != nil {
+		return fmt.Errorf("while close file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	return os.Remove(m.Fd.Name())
+}
+
+// Close would close the file. It would also truncate the file if maxSz >= 0.
+func (m *MmapFile) Close(maxSz int64) error {
+	// Badger can set the m.Data directly, without setting any Fd. In that case, this should be a
+	// NOOP.
+	if m.Fd == nil {
+		return nil
+	}
+	if err := m.Sync(); err != nil {
+		return fmt.Errorf("while sync file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	if err := Munmap(m.Data); err != nil {
+		return fmt.Errorf("while munmap file: %s, error: %v\n", m.Fd.Name(), err)
+	}
+	if maxSz >= 0 {
+		if err := m.Fd.Truncate(maxSz); err != nil {
+			return fmt.Errorf("while truncate file: %s, error: %v\n", m.Fd.Name(), err)
 		}
 	}
-	return sf, nil
+	return m.Fd.Close()
 }
 
-func (sf *SuperFlag) Has(opt string) bool {
-	val := sf.GetString(opt)
-	return val != ""
-}
-
-func (sf *SuperFlag) GetDuration(opt string) time.Duration {
-	val := sf.GetString(opt)
-	if val == "" {
-		return time.Duration(0)
-	}
-	if strings.Contains(val, "d") {
-		val = strings.Replace(val, "d", "", 1)
-		days, err := strconv.ParseInt(val, 0, 64)
-		if err != nil {
-			return time.Duration(0)
-		}
-		return time.Hour * 24 * time.Duration(days)
-	}
-	d, err := time.ParseDuration(val)
+func SyncDir(dir string) error {
+	df, err := os.Open(dir)
 	if err != nil {
-		return time.Duration(0)
+		return errors.Join(err, fmt.Errorf("while opening %s", dir))
 	}
-	return d
-}
-
-func (sf *SuperFlag) GetBool(opt string) bool {
-	val := sf.GetString(opt)
-	if val == "" {
-		return false
+	if err := df.Sync(); err != nil {
+		return errors.Join(err, fmt.Errorf("while syncing %s", dir))
 	}
-	b, err := strconv.ParseBool(val)
-	if err != nil {
-		err = errors.Join(err,
-			fmt.Errorf("Unable to parse %s as bool for key: %s. Options: %s\n", val, opt, sf))
-		log.Fatalf("%+v", err)
+	if err := df.Close(); err != nil {
+		return errors.Join(err, fmt.Errorf("while closing %s", dir))
 	}
-	return b
-}
-
-func (sf *SuperFlag) GetFloat64(opt string) float64 {
-	val := sf.GetString(opt)
-	if val == "" {
-		return 0
-	}
-	f, err := strconv.ParseFloat(val, 64)
-	if err != nil {
-		err = errors.Join(err,
-			fmt.Errorf("Unable to parse %s as float64 for key: %s. Options: %s\n", val, opt, sf))
-		log.Fatalf("%+v", err)
-	}
-	return f
-}
-
-func (sf *SuperFlag) GetInt64(opt string) int64 {
-	val := sf.GetString(opt)
-	if val == "" {
-		return 0
-	}
-	i, err := strconv.ParseInt(val, 0, 64)
-	if err != nil {
-		err = errors.Join(err,
-			fmt.Errorf("Unable to parse %s as int64 for key: %s. Options: %s\n", val, opt, sf))
-		log.Fatalf("%+v", err)
-	}
-	return i
-}
-
-func (sf *SuperFlag) GetUint64(opt string) uint64 {
-	val := sf.GetString(opt)
-	if val == "" {
-		return 0
-	}
-	u, err := strconv.ParseUint(val, 0, 64)
-	if err != nil {
-		err = errors.Join(err,
-			fmt.Errorf("Unable to parse %s as uint64 for key: %s. Options: %s\n", val, opt, sf))
-		log.Fatalf("%+v", err)
-	}
-	return u
-}
-
-func (sf *SuperFlag) GetUint32(opt string) uint32 {
-	val := sf.GetString(opt)
-	if val == "" {
-		return 0
-	}
-	u, err := strconv.ParseUint(val, 0, 32)
-	if err != nil {
-		err = errors.Join(err,
-			fmt.Errorf("Unable to parse %s as uint32 for key: %s. Options: %s\n", val, opt, sf))
-		log.Fatalf("%+v", err)
-	}
-	return uint32(u)
-}
-
-func (sf *SuperFlag) GetString(opt string) string {
-	if sf == nil {
-		return ""
-	}
-	return sf.m[opt]
-}
-
-func (sf *SuperFlag) GetPath(opt string) string {
-	p := sf.GetString(opt)
-	path, err := expandPath(p)
-	if err != nil {
-		log.Fatalf("Failed to get path: %+v", err)
-	}
-	return path
-}
-
-// expandPath expands the paths containing ~ to /home/user. It also computes the absolute path
-// from the relative paths. For example: ~/abc/../cef will be transformed to /home/user/cef.
-func expandPath(path string) (string, error) {
-	if len(path) == 0 {
-		return "", nil
-	}
-	if path[0] == '~' && (len(path) == 1 || os.IsPathSeparator(path[1])) {
-		usr, err := user.Current()
-		if err != nil {
-			return "", errors.Join(err, errors.New("Failed to get the home directory of the user"))
-		}
-		path = filepath.Join(usr.HomeDir, path[1:])
-	}
-
-	var err error
-	path, err = filepath.Abs(path)
-	if err != nil {
-		return "", errors.Join(err, errors.New("Failed to generate absolute path"))
-	}
-	return path, nil
+	return nil
 }
